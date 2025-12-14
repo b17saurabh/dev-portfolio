88dfe64d4953a9b9e9883df471f9cd023d0ced81
saurabh batham <saurabhbatham17@gmail.com>
2025-12-14 14:18:32 +0530

    Update Google Drive file ID for resume download link

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -10,7 +10,7 @@ export default function ResumeDownloadPage() {
   // Replace YOUR_FILE_ID with the actual Google Drive file ID
  
 
-  const googleDriveFileId = "1KpqIqC60Di_SBDiUUJH5cmg4N2JergI5";
+  const googleDriveFileId = "1cH-Z6mzudj354jaNOZTioGOtHZqYJ47E";
  
 
   const googleDriveLink = `https://drive.google.com/uc?export=download&id=${googleDriveFileId}`;
