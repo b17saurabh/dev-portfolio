49c436fdac9cac731a514b02ebcbd53510590d7f
Saurabh Batham <74095124+b17saurabh@users.noreply.github.com>
2026-02-25 16:16:39 +0530

    Update resume link

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -6,17 +6,13 @@ import { redirect } from "next/navigation";
 
 export default function ResumeDownloadPage() {
  
-
-  // Replace YOUR_FILE_ID with the actual Google Drive file ID
  
 
-  const googleDriveFileId = "1cH-Z6mzudj354jaNOZTioGOtHZqYJ47E";
+  const googleDriveFileId = "1rXufEWgmeIqbayYjruBnl0DAr-XxjZT9"
  
 
   const googleDriveLink = `https://drive.google.com/uc?export=download&id=${googleDriveFileId}`;
  
-
-
  
 
   redirect(googleDriveLink);
